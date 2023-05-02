73a6537c8900ce4e94659a05047dcf3c04fff43d
Ashlee Lindstrom <ashm0428@gmail.com>
2023-05-02 15:08:00 -0600

    a few minor updates

--- lessons/express-server/app.js
+++ lessons/express-server/app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
+const cors = require('cors');
 const express = require("express");
 const app = express();
 const practiceController = require("./controllers/practice.controller");
 const playerController = require("./controllers/player.controller");
  
+app.use(cors());
+
 app.use(express.json()); //! THIS WILL ALLOW YOU TO SEND IN YOUR PAYLOAD JSON OBJECT AND IT WILL PARSE IT FOR US - Middleware
 
 // http://localhost:4000/
